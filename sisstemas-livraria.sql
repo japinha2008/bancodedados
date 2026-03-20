3b7f531873b8b806046b9e617b0e73db8f209f13
japinha2008 <nicole.abdala@escola.pr.gov.br>
2026-03-20 07:44:01 -0300

    Update sisstemas-livraria.sql

--- sisstemas-livraria.sql
+++ sisstemas-livraria.sql
@@ -10,3 +10,19 @@ CREATE TABLE Compras (
     NomeLivro VARCHAR(255),
     FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
 );
+
+SELECT * FROM Clientes;
+-- Inserindo dados na tabela Clientes
+INSERT INTO Clientes (ID, nomeCliente, emailCliente)
+VALUES 
+(1, 'Ana Silva', 'ana.silva@email.com'),
+(2, 'Carlos Souza', 'carlos.souza@email.com'),
+(3, 'Mariana Oliveira', 'mariana.oliveira@email.com');
+
+-- Inserindo dados na tabela Compras
+INSERT INTO Compras (CompraID, ClienteID, NomeLivro)
+VALUES
+(101, 1, 'Dom Casmurro'),
+(102, 2, 'O Pequeno Príncipe'),
+(103, 3, '1984'),
+(104, 1, 'Memórias Póstumas de Brás Cubas');
